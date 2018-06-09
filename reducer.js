a760891a3889ffa573c2f34512a2bcbff553e512
Hoon Lee <yl.hoony@gmail.com>
2018-06-09 15:59:47 -0700

    followed code along

--- reducer.js
+++ reducer.js
@@ -1,19 +1,27 @@
-let state = {counter: 0};
+let state;
 
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+function changeState(state = { count: 0 }, action) {
+  console.log(state)
+  console.log(action)
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 }
+
+    default:
+      return state;
   }
-
-function dispatch(action){
-    state = changeState(state, action)
-    render()
 }
 
-function render(){
-    document.setInnerHtml = state.counter
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
 }
+
+function render() {
+  // document.innerHTML = state.count
+  console.log(state.count);
+}
+
+dispatch({ type: '@@INIT' })
+dispatch({ type: 'INCREASE_COUNT' })
+// dispatch({ type: '@@INIT' })
